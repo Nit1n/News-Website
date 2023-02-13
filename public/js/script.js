5cac43369a69930d3b7e0bb97af7b9c5c479a936
Navneetk-bst <navneet.kumar@bluestacks.com>
2023-02-14 01:06:10 +0530

    test change:

--- public/js/script.js
+++ public/js/script.js
@@ -6,6 +6,7 @@ console.log('script running') ;
 arr.forEach(function(option){
     option.selected = false ; 
    // console.log(option.value) ; 
+   
     if(option.value === value){
         option.selected = true ; 
     }       
